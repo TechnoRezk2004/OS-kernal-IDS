e6fb2294db21a99c8c354a57460717ebeeacc662
Rezk Youssef <163682043+TechnoRezk2004@users.noreply.github.com>
2026-04-20 00:47:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from monitor1 import b, handle_event
+import signal
+import sys
 
-b["events"].open_perf_buffer(handle_event)
-print("🔍 Monitoring started...")
+def signal_handler(sig, frame):
+    print("\n⛔ Monitoring stopped.")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+b["events"].open_perf_buffer(handle_event, page_cnt=64)
+print("🔍 Monitoring started... Press Ctrl+Z to stop")
+
 while True:
-    b.perf_buffer_poll()
+    b.perf_buffer_poll(timeout=100)
